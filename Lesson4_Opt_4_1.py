ee75b01bfbfcc98974eb549d6de7f4c1ddf1eb43
Ratatouille <307338947@qq.com>
2018-09-29 01:16:23 +0800

    第四课的代码

---
+++ Lesson4_Opt_4_1.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = 2
+import tensorflow as tf
+import numpy as ny
+BATCH_SIZE = 8
+seed = 23455
+
+rdm = tf.random.RandomState(seed)
+X = rdm.rand(32,2)
+Y = [[x1+x2+rdm.rand()/10.0-0.05] for (x1,x2) in X]
+
+x = tf.placeholder(tf.float32,shape(None,2))
+y_= tf.placeholder(tf.float32,shape(None,1))
+
+w1=tf.Variable(tf.random_normal([2,1],stddev=1,seed=1))
+
+y=tf.matmul(x,w1)
+
+loss_mse = tf.reduce_mean(tf.squared_difference(y,y_))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
+
+with tf.Session as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+
+    STEPS = 3000
+    for i in range(STEPS):
+        start = (i*BATCH_SIZE) % 32
+        end = start + BATCH_SIZE
+        sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]})
