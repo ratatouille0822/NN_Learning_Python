354d184e56b7125a224f47dd7f5a4775774f2d2c
Ratatouille <307338947@qq.com>
2018-09-29 01:32:04 +0800

    第四课的代码

--- Lesson4_Opt_4_1.py
+++ Lesson4_Opt_4_1.py
@@ -1,17 +1,17 @@
 #coding:utf-8
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = 2
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
-import numpy as ny
+import numpy as np
 BATCH_SIZE = 8
 seed = 23455
 
-rdm = tf.random.RandomState(seed)
+rdm = np.random.RandomState(seed)
 X = rdm.rand(32,2)
 Y = [[x1+x2+rdm.rand()/10.0-0.05] for (x1,x2) in X]
 
-x = tf.placeholder(tf.float32,shape(None,2))
-y_= tf.placeholder(tf.float32,shape(None,1))
+x = tf.placeholder(tf.float32,shape=(None,2))
+y_= tf.placeholder(tf.float32,shape=(None,1))
 
 w1=tf.Variable(tf.random_normal([2,1],stddev=1,seed=1))
 
@@ -20,7 +20,7 @@ y=tf.matmul(x,w1)
 loss_mse = tf.reduce_mean(tf.squared_difference(y,y_))
 train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
 
-with tf.Session as sess:
+with tf.Session() as sess:
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
 
@@ -30,7 +30,7 @@ with tf.Session as sess:
         end = start + BATCH_SIZE
         sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]})
         if i % 500 == 0:
-            total_loss = sess.run(train_step,feed_dict={x:X,y_:Y})
+            total_loss = sess.run(loss_mse,feed_dict={x:X,y_:Y})
             print("After %d train steps, total loss is %g"%(i,total_loss))
     print("w1 is : \n",sess.run(w1))
 
